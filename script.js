8c6b38959450d0bc8511bae90382440ad1560b5a
Leiserg <84937238+Leiserg@users.noreply.github.com>
2021-06-06 10:40:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,16 +7,10 @@ let isNumber = function(n) {
 
 function game() {
     const hiddenNumber = Math.floor(Math.random() * 100) + 1;
-    // let start = confirm('Загадывание случайного числа от 1 до 100');
-    // if (start === false) {
-    //     alert('Игра окончена');
-    // }
-
     function action() {
-        
         let guessNumber = +prompt('Угадай число от 1 до 100');
-
-        if (!isNumber(guessNumber) || guessNumber.trim === '') {
+        console.log(guessNumber);
+        if (!isNumber(guessNumber)) {
             alert('Введите число!');
             action();
         }else if (Boolean(guessNumber) === false) {
@@ -30,13 +24,10 @@ function game() {
             alert('Загаданное число больше');
             action();
         }
-        console.log(guessNumber);
-        console.log(hiddenNumber);
     }
     action();
 }
 
 game();
 
-console.dir(game);
 
