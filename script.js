3cb5f60a4ab18c7d129189cee089978f8779dd99
Leiserg <84937238+Leiserg@users.noreply.github.com>
2021-06-15 17:16:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 const seriesBooks = document.querySelectorAll('.books'),
     books = document.querySelectorAll('.book'),
-    book3 = document.querySelectorAll('#a'),
+    book3 = document.querySelectorAll('a'),
     advertisement = document.querySelector('.adv'),
     elementsBooks = document.querySelectorAll('ul'),
     elems = document.querySelectorAll('li');
+
+    advertisement.remove();
 
 seriesBooks[0].prepend(books[1]);
 books[1].after(books[0]);
@@ -15,13 +17,12 @@ seriesBooks[0].append(books[2]);
 
 document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 
-book3[4].replaceWith('Книга 3. this и Прототипы Объектов');
-
-advertisement.remove();
-
 elems[3].after(elems[6]);
 elems[6].after(elems[8]);
 elems[9].after(elems[2]);
 elems[50].after(elems[48]);
 elems[47].after(elems[55]);
 elems[53].after(elems[51]);
+
+
+book3[4].textContent = 'Книга 3. this и Прототипы Объектов';
