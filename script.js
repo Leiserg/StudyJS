f65a88e9c524e0c2e53028627b8094ec3119405c
Leiserg <84937238+Leiserg@users.noreply.github.com>
2021-06-22 19:35:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ DomElement.prototype.method1 = function(){
         } else if(this.selector[0] === '#'){
             const p = document.createElement('p');
             p.id = this.selector.slice(1);
+            p.style.cssText = `height: ${this.height + 'px'}; width: ${this.width + 'px'}; background: ${this.bg}; font-size: ${this.fontSize + 'px'}`;
             p.innerHTML = "Some text with <b>bold text</b>";
             document.body.appendChild(p);
             console.log(p);
@@ -50,3 +51,7 @@ DomElement.prototype.method1 = function(){
 let test1 = new DomElement('.start', 300, 300, 'red', 50);
 test1.method1();
 console.log(test1);
+
+let test2 = new DomElement('#start', 400, 400, 'blue', 60);
+test2.method1();
+console.log(test2);
