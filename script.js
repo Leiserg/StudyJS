8f436e60c0593637365e1a794baecc3cc8e3ce7f
Leiserg <84937238+Leiserg@users.noreply.github.com>
2021-06-22 19:16:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,20 +22,19 @@
 //     }
 
 
-class DomElement {
-    constructor (selector, height, width, bg, fontSize){
+function DomElement(selector, height, width, bg, fontSize) {
         this.selector = selector;
         this.height = height;
         this.width = width;
         this.bg = bg;
         this.fontSize = fontSize;
-    }
-    method1(){
+}
+DomElement.prototype.method1 = function(){
         if (this.selector[0] === '.'){
             const div = document.createElement('div');
             div.className = this.selector.slice(1);
             div.innerHTML = "Some text with <b>bold text</b>";
-            div.style.cssText = 'height: height; width: width; background: bg; fontSize: fontSize;';
+            div.style.cssText = `height: ${this.height + 'px'}; width: ${this.width + 'px'}; background: ${this.bg}; font-size: ${this.fontSize + 'px'}`;
             document.body.appendChild(div);
             console.log(div);
         } else if(this.selector[0] === '#'){
@@ -45,10 +44,9 @@ class DomElement {
             document.body.appendChild(p);
             console.log(p);
         }        
-    }
-}
+};
 
 
-let car1 = new DomElement('.start', 100, '2000px', 'red', 50);
-
-console.log(car1);
+let test1 = new DomElement('.start', 300, 300, 'red', 50);
+test1.method1();
+console.log(test1);
