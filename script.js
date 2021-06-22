dd8a2782f7a325cfa2aef3674c80fd9848fdc070
Leiserg <84937238+Leiserg@users.noreply.github.com>
2021-06-22 20:37:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 'use strict';
 
-
-// let selector = '.start';
-// if (selector[0] === '.'){
-//         const div = document.createElement('div');
-//         let style = getComputedStyle(div);
-//         let heigh = style.height;
-//         div.className = selector.slice(1);
-//         div.style.cssText = 'height: height; width: 300px; background: blue; font-size: 50px;';
-//         div.innerHTML = "Some text with bold text";
-//         document.body.appendChild(div);
-//         console.log(div);
-//         // console.log(style);
-//         console.log(heigh.height);
-//     } else if(selector[0] === '#'){
-//         const p = document.createElement('p');
-//         p.id = selector.slice(1);
-//         p.innerHTML = "Some text with <b>bold text</b>";
-//         document.body.appendChild(p);
-//         console.log(p);
-//     }
-
-
 function DomElement(selector, height, width, bg, fontSize) {
         this.selector = selector;
         this.height = height;
